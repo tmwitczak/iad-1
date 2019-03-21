3771a60a992685d78d7574074effeaeb39c59242
Kamil Kowalewski <szamil128@onet.pl>
2019-03-21 19:34:02 +0100

    next commit

--- main.py
+++ main.py
@@ -12,18 +12,9 @@ def main():
     abaloneData = inputData.readFile("data\_abalone.data")
     wineData = inputData.readFile("data\_wine.data")
 
-    # # inputing parameters from user
-    # kValue = input("Podaj wartosc K: ")
-    # tolerance = input("Podaj wartosc tolerancji: ")
-    # maxNumOfIter = input("Podaj maksymalna liczbe iteracji: ")
-    # kMeans = KMeansAlgorithm(kValue, tolerance, maxNumOfIter)
-
-    # plt.plot(irisData.iloc[:, 0], irisData.iloc[:, 1], "r+", label="Irysy")
-    # plt.show()
-    # # plt.plot(abaloneData.iloc[:, 2], seedsData.iloc[:, 3], "r+", label="Abalone")
-    # # plt.show()
-    # plt.plot(wineData.iloc[:, 0], wineData.iloc[:, 1], "r+", label="Wina")
-    # plt.show()
+    # inputing parameters from user
+    kValue = input("Podaj wartosc K: ")
+    kMeans = KMeansAlgorithm(kValue)
 
 
 # /////////////////////////////////////////////////////////// Execute program #
--- src/KMeansAlgorithm.py
+++ src/KMeansAlgorithm.py
@@ -2,12 +2,5 @@ import numpy as np
 
 
 class KMeansAlgorithm:
-    def __init__(self, kValue, tolerance, maxNumOfIter):
+    def __init__(self, kValue):
         self.kValue = kValue
-        self.tolerance = tolerance
-        self.maxNumOfIter = maxNumOfIter
-
-    def Euclidean_distance(firstPoint, secondPoint):
-        distance = np.ligalg.norm(firstPoint, secondPoint)
-        return distance
-
